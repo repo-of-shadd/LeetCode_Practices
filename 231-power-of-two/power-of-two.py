ecddab873c04c1b5a723aa5660d07166a6c05968
Md. Shadd <md.shadd@gmail.com>
2024-02-20 02:24:38 +0600

    Time: 31 ms (87.93%) | Memory: 16.4 MB (95.87%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,4 +1,3 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        # return True if n == 1 or math.log2(n) % 1 == 0 else False
         return False if n <= 0 else n == 1 or math.log2(n) % 1 == 0
