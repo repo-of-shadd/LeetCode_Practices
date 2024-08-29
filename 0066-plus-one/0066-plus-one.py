6540f95948217af3ef4c8a37cb23a87622f87adc
Md. Shadd <md.shadd@gmail.com>
2024-08-29 19:56:11 +0600

    Time: 32 ms (82.53%), Space: 16.3 MB (99.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,5 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        number = int(''.join(map(str, digits)))
-        output = number + 1
-        return list(map(int, str(output)))
+        
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+                if i == 0:
+                    return [1] + digits
+            
