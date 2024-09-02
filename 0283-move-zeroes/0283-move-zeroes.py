2fdadde2e284b40e347f2c6f85526e21141fb8cc
Md. Shadd <md.shadd@gmail.com>
2024-09-02 16:20:11 +0600

    Time: 772 ms (9.28%), Space: 18 MB (60.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,11 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                nums.remove(0)
+        for i in nums:
+            if i == 0:
+                nums.pop(nums.index(i))
                 nums.append(0)
-
-
-
-                
